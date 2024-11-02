d7e3e6adfb7bde0929e6e92b1385f75b13458ccc
Julio <julionev10@gmail.com>
2024-11-02 17:14:37 -0300

    Initial commit

---
+++ src/test/java/br/com/julioneves/todolist/TodolistApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.julioneves.todolist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodolistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
